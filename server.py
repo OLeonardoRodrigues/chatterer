f77b9e6bdeee7d18abd4cb9ebca6c94a6d2a40f9
OliveiraLeonardo17 <oliveira.leonardo.r@gmail.com>
2020-04-21 18:49:59 -0300

    create client.py and server.py

--- server.py
+++ server.py
@@ -1,2 +1,43 @@
-from socket import AF_INET, socket, SOCK_STREAM
-from threading import Thread
+import socket, threading
+from _thread import *
+
+print_lock = threading.Lock()
+
+def threaded(c):
+    while True:
+        data = c.recv(1024)
+        if not data:
+            print('Bye')
+            print_lock.release
+            break
+        data = data[::-1]
+        c.send(data)
+        c.close()
+
+def Main():
+    host= ''
+
+    port=12345
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.bind((host, port))
+
+    print(f'Socket binded to port: {port} ')
+
+    s.listen(5)
+    print('Socket is listening... ')
+
+    while True:
+        c, addr = s.accept()
+
+        print_lock.acquire()
+        print(f'Connected to {addr[0]}:{addr[1]}')
+
+        start_new_thread(threaded, (c,))
+
+    s.close()
+
+if __name__ == '__main__':
+    Main()
+
